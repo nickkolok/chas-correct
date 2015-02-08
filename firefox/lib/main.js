188b3ddb8742805af6a6f8e8e05ad4db0783b33d
nickkolok <nickkolok@mail.ru>
2015-02-08 23:40:36 +0300

    Вариант под firefox

---
+++ firefox/lib/main.js
@@ -0,0 +1,15 @@
+// main.js
+
+var data = require("sdk/self").data;
+var pageMod = require("sdk/page-mod");
+
+pageMod.PageMod({
+	include: "*",
+	contentScriptFile: [
+		data.url("jquery-2.1.0.min.js"	),
+		data.url("jstorage.min.js"		),
+		data.url("dictionary.js"		),
+		data.url("content.js"			),
+	],
+});
+
