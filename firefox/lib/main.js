ef859718c5a5e03e4a4402bcbbfe9598d0f4026a
Aleksey Porfirov <lexqt@yandex.ru>
2015-08-30 19:28:06 +0300

    Use ActionButton (Firefox extension)

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,16 +1,23 @@
 // main.js
 
 var data = require("sdk/self").data;
-var pageMod = require("sdk/page-mod");
+var tabs = require("sdk/tabs");
+var ui = require("sdk/ui");
 
-pageMod.PageMod({
-	include: "*",
-	contentScriptFile: [
-		data.url("jquery-2.1.0.min.js"	),
-		data.url("jstorage.min.js"		),
-		data.url("dictionary.js"		),
-		data.url("prepareDictionary.js"		),
-		data.url("content.js"			),
-	],
+var actionButton = ui.ActionButton({
+  id: "chas-correct",
+  label: "chas-correct",
+  icon: data.url("icon.png"),
+
+  onClick: function(state) {
+    tabs.activeTab.attach({
+      contentScriptFile: [
+        data.url("jquery-2.1.0.min.js"),
+        data.url("jstorage.min.js"),
+        data.url("dictionary.js"),
+        data.url("prepareDictionary.js"),
+        data.url("content.js"),
+      ],
+    });
+  }
 });
-
