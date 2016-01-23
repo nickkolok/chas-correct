8b42a5a604397d0d69492a78eb539fbb58c48e8a
nickkolok <nickkolok@mail.ru>
2016-01-24 00:10:13 +0300

    #32  Работа с дампами

--- parser/urllist.js
+++ parser/urllist.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var parser = require('./parse-lib.js');
 var childProcess=require('child_process');
+var zlib=require('zlib');
 var wordcounterProcess = childProcess.fork(__dirname+'/wordcounterProcess.js');
 var     checkerProcess = childProcess.fork(__dirname+    '/checkerProcess.js');
 
-var zlib=require('zlib');
+
+var dateRelative=1453576000000-40*1000; //Магическая константа, чтобы время в дампе меньше места занимало
 
 var globalExpression=parser.makeGlobalExpression();
 var actionArray=parser.readActionArray();
@@ -19,12 +21,35 @@ var log404="";
 
 var htmlTable='';
 
-var dump={};
+var dump={data:{}};
+var requestsSent=0;
+
+
+function readDump(){
+	try{
+		var gz=fs.readFileSync('dumps/'+name+'.dump.json.gz');
+		var json=zlib.gunzipSync(gz);
+		dump=JSON.parse(json);
+		if(!dump.data){
+			dump.data={};
+		}
+		console.log('Дамп прочитан успешно, содержит URL: '+Object.keys(dump.data).length);
+	}catch(e){
+		console.error('Не удалось прочитать дамп '+'dumps/'+name+'.dump.json.gz');
+		console.error(e);
+	}
+}
 
 function getURLfromDumpOrHttp(beginFrom,endWith,newopts){
-	setTimeout(function(){
-		parser.getChunkFromURL(newopts.url,workWithChunk,beginFrom,endWith,newopts);
-	},(newopts.pause||100)*newopts.i);
+	if(dump.data[newopts.url] && (dump.data[newopts.url][0]!='')){
+		newopts.fromDump=1;
+		workWithChunk(dump.data[newopts.url][0],newopts);
+	} else {
+		setTimeout(function(){
+			parser.getChunkFromURL(newopts.url,workWithChunk,beginFrom,endWith,newopts);
+		},(newopts.pause||100)*requestsSent);
+		requestsSent++;
+	}
 }
 
 function countErrorsInURLarray(urls,maxlength,beginFrom,endWith,options){
@@ -36,14 +61,8 @@ function countErrorsInURLarray(urls,maxlength,beginFrom,endWith,options){
 		name=options.name;
 	}
 	console.log("Обрабатывается страниц: "+length);
-	for(var i=0; i<length; i++){
-		var newopts={};
-		for(var prop in options)
-			newopts[prop]=options[prop];
-		newopts.url=urls[i];
-		newopts.i=i;
-		getURLfromDumpOrHttp(beginFrom,endWith,newopts);
-	}
+	readDump();
+	setInterval(flushDump,300*1000);
 	checkerProcess.send({
 		type:  'init',
 		left:  '[^\.!?|]*',
@@ -60,6 +79,7 @@ function countErrorsInURLarray(urls,maxlength,beginFrom,endWith,options){
 					'<td><a href="'+m.options.url+'">'+m.options.url.replace(/^https+\:\/\//,'')+'</a></td>'+
 					'<td>'+m.text+'</td>'+
 					'<td>'+m.signatures.join(' ; ')+'</td>'+
+					'<td>'+new Date(dump.data[m.options.url][1]*60000+dateRelative).toLocaleString()+'</td>'+
 				'</tr>';
 			break;
 			case 'quantity':
@@ -67,6 +87,15 @@ function countErrorsInURLarray(urls,maxlength,beginFrom,endWith,options){
 			break;
 		}
 	});
+
+	for(var i=0; i<length; i++){
+		var newopts={};
+		for(var prop in options)
+			newopts[prop]=options[prop];
+		newopts.url=urls[i];
+		newopts.i=i;
+		getURLfromDumpOrHttp(beginFrom,endWith,newopts);
+	}
 }
 
 function countErrorsInURLlist(filename,maxlength,beginFrom,endWith,options){
@@ -76,8 +105,15 @@ function countErrorsInURLlist(filename,maxlength,beginFrom,endWith,options){
 }
 
 function workWithGoodChunk(text,options){
+//	console.log(text);
 //	text=text.replace(/[^а-яё]{4,}/gi,";");
 	text=text.replace(/<[^>]*>/gi,"|").replace(/<\s+>/g," ");
+	if(!options.fromDump){
+		dump.data[options.url]=[//Массив - чтобы меньше места занимало
+			text,
+			Math.round((Date.now()-dateRelative)/60000),//С точностью до минут и фиксированным смещением - для того же
+		];
+	}
 	if(!text.length){
 		log404+=(options.url+" : целевой текст не выделен\n");
 		pagesWithErrors++;
@@ -95,8 +131,7 @@ function workWithGoodChunk(text,options){
 	});
 }
 function workWithChunk(text,options){
-	console.error(pagesProceeded,text.length,options);
-//	dump[options.url]=text;
+	console.error(pagesProceeded+1,text.length,options);
 	workWithGoodChunk(text,options);
 	pagesProceeded++;
 	if(pagesProceeded==length){
@@ -117,15 +152,16 @@ function finishCheck(){
 		type: 'finish',
 		filename: 'results/'+name,
 	});
+	flushDump();
+}
 
-//	fs.writeFile("results/"+name+".dump.json",JSON.stringify(dump));
-/*
-	zlib.deflate(JSON.stringify(dump), function(err, buffer) {
+function flushDump(){
+	zlib.gzip(JSON.stringify(dump), function(err, buffer) {
 		if (!err) {
-			console.log(buffer.length);
+			console.log('Размер дампа: '+buffer.length);
+			fs.writeFile("dumps/"+name+".dump.json.gz",buffer);
 		}
-	});
-*/
+	},{ windowBits: 21, memLevel: 14, level: 9, });
 }
 
 function printNumbers(mistakes){
@@ -144,7 +180,7 @@ function printNumbers(mistakes){
 	bothLog("Ошибок на 1000 словоупотреблений обнаружено: "+(mistakes/wordsCount*1000));
 	bothLog("В среднем одна ошибка на "+(wordsCount/mistakes)+" словоупотреблений");
 	html+='<table cellpadding="5" cellspacing="0" border="1">'+
-		'<tr><th>Адрес</th><th>Контекст</th><th>Сигнатуры</th></tr>'+
+		'<tr><th>Адрес</th><th>Контекст</th><th>Сигнатуры</th><th>Обновлено</th></tr>'+
 		htmlTable+
 		'</table></body></html>';
 	fs.writeFileSync('results/'+name+'.report.html',html);
