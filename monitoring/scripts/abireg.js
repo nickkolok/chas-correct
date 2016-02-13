7d25775a7ae70f3d42025e0166319eea9394d46a
nickkolok <nickkolok@mail.ru>
2016-02-13 11:46:51 +0300

    abireg.ru - добавлены ссылки на 5 дополнительных дампов (ибо да, глючит), уточнён диапазон страниц

--- monitoring/scripts/abireg.js
+++ monitoring/scripts/abireg.js
@@ -1,7 +1,12 @@
 var urllist = require('../../parser/urllist.js');
 //abireg.ru - тут сплошная нумерация
 var abireglinks=[];
-for(var i=2000; i<55000; i++){
+for(var i=2000; i<52000; i++){
 	abireglinks.push('http://www.abireg.ru/n_'+i+'.html');
 }
-urllist.countErrorsInURLarray(abireglinks,100000,'<td class=news>','<div class=print>',{encoding:'win1251',name:'abireg',pause:500});
+urllist.countErrorsInURLarray(abireglinks,100000,'<td class=news>','<div class=print>',{
+	encoding:'win1251',
+	name:'abireg',
+	pause:500,
+	additionalDumps:['abireg.1','abireg.2','abireg.3','abireg.4','abireg.5'],
+});
