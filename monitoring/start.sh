63b07c4cfad54c050a626a2550c66d2b3de66a43
nickkolok <nickkolok@mail.ru>
2016-03-23 19:31:08 +0300

    Убрано перенаправление результатов работы скриптов мониторинга в файл, т. к. всё равно логи программно пишутся

--- monitoring/start.sh
+++ monitoring/start.sh
@@ -3,5 +3,6 @@ ulimit -n 4096 #Костыль
 mkdir results
 for script in `cd scripts && ls *.js && cd ..`
 do
-	ionice -c3 nice -n9 js scripts/$script > results/$script.common.log &
+#	ionice -c3 nice -n9 js scripts/$script > results/$script.common.log &
+	ionice -c3 nice -n9 js scripts/$script
 done;
