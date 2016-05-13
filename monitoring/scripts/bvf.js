404f435efdf9938a6d4bf3624593cbd25df47187
nickkolok <nickkolok@mail.ru>
2016-05-14 00:24:49 +0300

    Уточнён дампер БВФ

--- monitoring/scripts/bvf.js
+++ monitoring/scripts/bvf.js
@@ -1,21 +1,24 @@
 var urllist = require('../../parser/urllist.js');
 //bvf.ru - тут сплошная нумерация
 var bvflinks=[];
-for(var i=0; i<6000; i++){
+for(var i=700/*1096635*/; i<1096646; i++){
 	bvflinks.push('http://bvf.ru/forum/showthread.php?t='+i);
 }
-urllist.countErrorsInURLarray(bvflinks,100000,
+urllist.countErrorsInURLarray(bvflinks,82000/*10000000*/,
 //	'<!-- / end content table -->',
 //	'<!-- / thread tools menu -->',
-	'<body',
-	'</body',
+//	'<body',
+//	'jQuery.getScript(\'http://service.bvf.ru/listen/bvfmenuv2/?userid=0&securitytoken=guest\');',
+	'<div id="posts">',
+	'function insertQuote',
+//	'</body',
 	{
 		encoding:'win1251',
 		name:'bvf',
 		pause:500,
-		additionalDumps:[
+/*		additionalDumps:[
 			'bvf.1',
 			'bvf.2',
-		],
+		],*/
 	}
 );
