44641747ef0bccfb15d84cc05ba07504f39c207b
nickkolok <nickkolok@mail.ru>
2017-10-09 02:25:17 +0300

    Вероятно, это быстрорефакторинг, но сейчас уже не помню

--- monitoring/scripts/bvf.js
+++ monitoring/scripts/bvf.js
@@ -4,21 +4,12 @@ var bvflinks=[];
 for(var i=700/*1096635*/; i<1096646; i++){
 	bvflinks.push('http://bvf.ru/forum/showthread.php?t='+i);
 }
-urllist.countErrorsInURLarray(bvflinks,82000/*10000000*/,
-//	'<!-- / end content table -->',
-//	'<!-- / thread tools menu -->',
-//	'<body',
-//	'jQuery.getScript(\'http://service.bvf.ru/listen/bvfmenuv2/?userid=0&securitytoken=guest\');',
+urllist.countErrorsInURLarray(bvflinks,10000/*82000*//*10000000*/,
 	'<div id="posts">',
 	'function insertQuote',
-//	'</body',
 	{
 		encoding:'win1251',
 		name:'bvf',
 		pause:500,
-/*		additionalDumps:[
-			'bvf.1',
-			'bvf.2',
-		],*/
 	}
 );
