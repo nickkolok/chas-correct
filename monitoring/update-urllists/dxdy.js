38026570882829bfdb4d52d7246797a1c7c069b6
nickkolok <nickkolok@mail.ru>
2017-10-11 23:45:16 +0300

    Увеличено количество изучемых подфорумов на dxdy.ru - Ксения Шевелёва

--- monitoring/update-urllists/dxdy.js
+++ monitoring/update-urllists/dxdy.js
@@ -9,9 +9,20 @@ var prefixes = [
 	'http://dxdy.ru/matematika-obschie-voprosy-f1-',
 	'http://dxdy.ru/analiz-i-f65-',
 	'http://dxdy.ru/pomogite-reshit-razobratsya-m-f27-',
-//	'',
-//	'',
-//	'',
+	'http://dxdy.ru/analiz-ii-f66-',
+	'http://dxdy.ru/veroyatnost-statistika-f67-',
+	'http://dxdy.ru/vysshaya-algebra-f68-',
+	'http://dxdy.ru/diskretnaya-matematika-kombinatorika-teoriya-chisel-f69-',
+	'http://dxdy.ru/mat-logika-osnovaniya-matematiki-teoriya-algoritmov-f70-',
+	'http://dxdy.ru/chislennye-i-vychislitelnye-metody-optimizatsiya-f71-',
+	'http://dxdy.ru/geometriya-f73-',
+	'http://dxdy.ru/shkolnaya-algebra-f72-',
+	'http://dxdy.ru/prochee-f74-',
+	'http://dxdy.ru/olimpiadnye-zadachi-m-f26-',
+	'http://dxdy.ru/velikaya-teorema-ferma-f62-',
+	'http://dxdy.ru/diskussionnye-temy-m-f28-',
+	'http://dxdy.ru/internet-resursy-m-f36-',
+	'http://dxdy.ru/matematicheskij-spravochnik-f52-',
 ];
 
 
@@ -24,7 +35,7 @@ for (var i = 0; i < prefixes.length; i++) {
 		postfix: '0.html',
 		linkpattern:	'http://dxdy.ru/topic',
 		linkprefix:	'',
-		pause:2000,
+		pause:5000,
 	});
 }
 
