66a423507fec3f42bb81e78436ed060f8a4192b3
nickkolok <nickkolok@mail.ru>
2017-10-11 23:47:15 +0300

    Скрипт кучного запуска всей математики

---
+++ monitoring/start-math.sh
@@ -0,0 +1,22 @@
+ulimit -n 4096 #Костыль
+mkdir results
+
+for site in \
+	matematikalegko-comments \
+	matematikalegko \
+	ege-ok \
+	ege-ok-comments \
+	dxdy \
+	mathprofi-ru \
+	nuru-ru \
+	ru-math-wikia-com \
+	1cov-edu_ru \
+	cleverstudents \
+	hijos_ru__comments \
+	hijos_ru \
+	algebraclass_ru \
+	forum_exponenta_ru \
+	ru_solverbook_com \
+; do
+	nohup ionice -c3 nice -n9 js scripts/$site.js &
+done
