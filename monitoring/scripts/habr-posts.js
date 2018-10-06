085e7f5164421a495cbaac206e5b466d0842f2a2
NickKolok <nickkolok@mail.ru>
2018-10-06 14:15:34 +0300

    Habr posts: new delimiters

--- monitoring/scripts/habr-posts.js
+++ monitoring/scripts/habr-posts.js
@@ -1,8 +1,17 @@
 var urllist = require('../../parser/urllist.js');
 //habrahabr.ru - тут сплошная нумерация. Только статьи
 var habrlinks=[];
-for(var i=170000; i<173000; i++){
+for(var i=100000; i<500000; i++){
 	habrlinks.push('http://habrahabr.ru/post/'+i+'/');
 }
-urllist.countErrorsInURLarray(habrlinks,10000000000,'<div class="content html_format">','<ul class="tags icon_tag">',{name:'habr-posts'});
+urllist.countErrorsInURLarray(
+	habrlinks,
+	10000000000,
+	'<span class="post__title-text">',
+	'<form action="/json/favorites/"',
+	{
+		name:'habr-posts',
+		pause:3000,
+	}
+);
 
