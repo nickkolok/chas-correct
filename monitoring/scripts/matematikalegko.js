d793296b89eb30851845404b0cafc703922f91ce
NickKolok <nickkolok@mail.ru>
2018-10-27 20:22:49 +0300

    [monitoring] matematikalegko 0 updated parser

--- monitoring/scripts/matematikalegko.js
+++ monitoring/scripts/matematikalegko.js
@@ -2,10 +2,11 @@ var urllist = require('../../parser/urllist.js');
 //matematikalegko.ru - классический WordPress
 urllist.countErrorsInURLlist(
 	'urllists/matematikalegko.urllist.json',
-	100000,
-	'<div class="postmeta">',
-	'<div class="postinfo">', {
-		pause:1000,
+	200000,
+	/(<h1 itemprop="headline">)|<body/i,
+	/<ol class="commentlist">|<\/body/i,
+	{
+		pause:10000,
 		name: 'matematikalegko',
 	}
 );
