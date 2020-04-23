7010e9c29a54eb9cbbe67a215ddf8565826bf7e5
aezv <62520157+aezv@users.noreply.github.com>
2020-04-23 13:37:41 +0300

    QUnit tests [corrections[2]]

--- test/test.js
+++ test/test.js
@@ -1,25 +1,15 @@
 var actionArray = require('../chrome/prepareDictionary.js').actionArray;
 var testDictionary = require('./testDictionary.js').testDictionary;
 
-function tested(fDictionary, fActionArray)
-{
-        var countFActionArray = 0;
-        while(countFActionArray < fActionArray.length)
-        {
-            fDictionary[0] = fDictionary[0].replace(fActionArray[countFActionArray][0], fActionArray[countFActionArray][1]);
-            countFActionArray++;
-        }
-            return fDictionary[0] == fDictionary[1] ?  true : false;
+function testing(fDictionary, fActionArray) {
+    for (var i = 0; i < fActionArray.length; i++)
+        fDictionary[0] = fDictionary[0].replace(fActionArray[i][0], fActionArray[i][1]);
+    return fDictionary[0] == fDictionary[1] ? true : false;
 }
 
-QUnit.test( "QUnit test: checking words from testDictionary.js", function(errorMessage){
-    var count = 0;
-    while(count < testDictionary.length)
-    {
-        errorMessage.ok(tested(testDictionary[count], actionArray),
-        "Ошибка в слове testDictionary[" + count + "]: [" + testDictionary[count] + "]");
-        count++;
-    }
+QUnit.test("QUnit test: checking words from testDictionary.js", function (errorMessage) {
+    for (var i = 0; i < testDictionary.length; i++)
+        errorMessage.ok(testing(testDictionary[i], actionArray), "Ошибка в слове testDictionary[" + i + "]: [" + testDictionary[i] + "]");
 });
 
 //для запуска в консоле использовал: qunit test.js
