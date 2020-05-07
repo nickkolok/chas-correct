a378acc3afdd437bc08b8e72b29716133ed4c6aa
aezv <62520157+aezv@users.noreply.github.com>
2020-05-07 03:04:54 +0300

    QUnit tests [corrections[4]]

--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
 var actionArray = require('../chrome/prepareDictionary.js').actionArray;
 var testDictionary = require('./testDictionary.js').testDictionary;
 
-function testing(fDictionary, fActionArray) {
-    for (var i = 0; i < fActionArray.length; i++)
-        fDictionary[0] = fDictionary[0].replace(fActionArray[i][0], fActionArray[i][1]);
+function testing(fDictionary) {
+    actionArray.map(function (fActionArray) {
+        fDictionary[0] = fDictionary[0].replace(fActionArray[0], fActionArray[1]);
+    });
     return fDictionary[0] == fDictionary[1];
 }
 
 QUnit.test("QUnit test: checking words from testDictionary.js", function (errorMessage) {
     for (var i = 0; i < testDictionary.length; i++)
-        errorMessage.ok(testing(testDictionary[i], actionArray), "Ошибка в слове testDictionary[" + i + "]: [" + testDictionary[i] + "]");
+        errorMessage.ok(testing(testDictionary[i]), "Ошибка в слове testDictionary[" + i + "]: [" + testDictionary[i] + "]");
 });
-
-//для запуска в консоле использовал: qunit test.js
