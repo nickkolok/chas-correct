1adfbdec6bcb161b763af888737b2f779999e74b
AlEvZV <aezv.global@gmail.com>
2020-09-08 23:06:26 +0300

    [monitoring] updating exceptions: calcs_su

--- monitoring/update-urllists/calcs_su.js
+++ monitoring/update-urllists/calcs_su.js
@@ -2,11 +2,15 @@ var LinkExtractor = require('../../parser/linkExtractor.js').LinkExtractor;
 
 var extractor = new LinkExtractor({
 	flushEvery: 10000,
-	name:	'calcs_su',
+	name: 'calcs_su',
 });
 
 
 extractor.extractURLlistFromSiteRecursive({
-	pause:5000,
+	pause: 5000,
 	root: 'http://calcs.su/',
+	exclude: [
+		'http://calcs.su/de/',
+		/https:\/\/calcs.su\/html\/calcs\/region\/(?!khabarovsk-krai)/
+	]
 });
