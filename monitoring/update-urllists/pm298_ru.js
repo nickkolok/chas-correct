4b14df46c8f02d3b6d690c89cede379999fb3048
AlEvZV <aezv.global@gmail.com>
2020-09-27 01:01:38 +0300

    [update-urllists] add pm298_ru

---
+++ monitoring/update-urllists/pm298_ru.js
@@ -0,0 +1,12 @@
+var LinkExtractor = require('../../parser/linkExtractor.js').LinkExtractor;
+
+var extractor = new LinkExtractor({
+	flushEvery: 10000,
+	name: 'pm298_ru',
+});
+
+
+extractor.extractURLlistFromSiteRecursive({
+	pause: 3000,
+	root: 'http://www.pm298.ru/',
+});
