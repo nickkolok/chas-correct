d7d323f2836ba30be26a583a24f9812981dec00c
AlEvZV <aezv.global@gmail.com>
2020-10-07 15:17:13 +0300

    [scripts] update script: treugolniki_ru

--- monitoring/scripts/treugolniki_ru.js
+++ monitoring/scripts/treugolniki_ru.js
@@ -6,7 +6,7 @@ urllist.countErrorsInURLlist(
 	'urllists/' + name + '.urllist.json',
 	1000000,
 	/(<div id="content">)/i,
-	/(<div id="comments">)/i,
+	/((<div id="comments">)|(<footer id="footer">))/i,
 	{
         pause: 5000,
         name: name,
