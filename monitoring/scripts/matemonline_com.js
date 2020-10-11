f0bd9d43258871eeca01977231c8ecc748cd8aaf
AlEvZV <aezv.global@gmail.com>
2020-10-11 16:11:57 +0300

    [monitoring/scripts] add matemonline_com

---
+++ monitoring/scripts/matemonline_com.js
@@ -0,0 +1,14 @@
+var urllist = require('../../parser/urllist.js');
+
+var name = 'matemonline_com';
+
+urllist.countErrorsInURLlist(
+	'urllists/' + name + '.urllist.json',
+	1000000,
+	/(<div class="content">)/i,
+	/(<div id="comments">)|(<div id="commentsAdd">)|(<div id="footer">)|(<\/body>)/i,
+	{
+        pause: 2000,
+        name: name,
+	}
+);
