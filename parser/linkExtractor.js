61ff5348284aa7591026b88d2e8cb573b67a18e7
AlEvZV <aezv.global@gmail.com>
2020-10-20 18:07:14 +0300

    [parser] exception swf

--- parser/linkExtractor.js
+++ parser/linkExtractor.js
@@ -88,7 +88,7 @@ LinkExtractor.prototype.writeExtractedURLsArray = function(array){
 
 function isNonParseableFormat(url){
 	return (
-		/\.(zip|rar|exe|jpe?g|png|mp3|gif|svg|djvu|pdf|od.|rtf|docx?|xlsx?|pptx?|ppsx?|dotx?|xltx?)$/i.test(url)
+		/\.(zip|rar|exe|jpe?g|png|mp3|gif|svg|swf|djvu|pdf|od.|rtf|docx?|xlsx?|pptx?|ppsx?|dotx?|xltx?)$/i.test(url)
 	);
 }
 
