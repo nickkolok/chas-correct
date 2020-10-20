9c832365014335051a1164d6a775d5d63719040b
AlEvZV <aezv.global@gmail.com>
2020-10-20 22:00:14 +0300

    [monitoring/scripts] editing the search interval: matemonline_com

--- monitoring/scripts/matemonline_com.js
+++ monitoring/scripts/matemonline_com.js
@@ -5,7 +5,7 @@ var name = 'matemonline_com';
 urllist.countErrorsInURLlist(
 	'urllists/' + name + '.urllist.json',
 	1000000,
-	/(<div class="content">)/i,
+	/(<div class="single_page">)|(<div id="post-)|(<html xmlns)/i,
 	/(<div id="comments">)|(<div id="commentsAdd">)|(<div id="footer">)|(<\/body>)/i,
 	{
         pause: 2000,
