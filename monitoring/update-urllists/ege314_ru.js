0f69a530381e9af182eb778d4441700f8d007a8a
AlEvZV <aezv.global@gmail.com>
2020-10-21 17:40:31 +0300

    [monitoring/update-urllists] add ege314_ru - close #102

---
+++ monitoring/update-urllists/ege314_ru.js
@@ -0,0 +1,20 @@
+var LinkExtractor = require('../../parser/linkExtractor.js').LinkExtractor;
+
+var extractor = new LinkExtractor({
+	flushEvery: 5000,
+	name: 'ege314_ru',
+});
+
+
+extractor.extractURLlistFromURLsequence({
+	pagesCount: 22,
+	reportEvery: 10000,
+	flushEvery: 10000,
+	prefix: 'https://ege314.ru/sitemap/',
+	linkpattern: 'https://ege314.ru/',
+	linkprefix: '',
+	pause: 2000,
+	exclude: [
+		'https://ege314.ru/sitemap/',
+	]
+});
