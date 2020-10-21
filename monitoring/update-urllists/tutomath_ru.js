9326c78399430b0aa00c0553bed12aabe5a663d3
AlEvZV <aezv.global@gmail.com>
2020-10-21 16:42:36 +0300

    [monitoring/update-urllists] add tutomath_ru - close #94

---
+++ monitoring/update-urllists/tutomath_ru.js
@@ -0,0 +1,21 @@
+var LinkExtractor = require('../../parser/linkExtractor.js').LinkExtractor;
+
+var extractor = new LinkExtractor({
+    flushEvery: 1000,
+    name: 'tutomath_ru',
+});
+
+
+extractor.extractURLlistFromURLsequence({
+    pagesCount: 14,
+    reportEvery: 10000,
+    flushEvery: 10000,
+    prefix: 'https://tutomath.ru/page/',
+    linkpattern: 'https://tutomath.ru/',
+    linkprefix: '',
+    pause: 2000,
+    exclude: [
+        '/category/',
+        '/page/',
+    ]
+});
