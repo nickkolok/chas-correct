8ed1d1182e672c08752056992b848b7d228e1f75
aezv <aezv.global@gmail.com>
2021-04-28 13:59:51 +0300

    [monitoring] Обновление скрипта сбора url'ов для kommersant-vrn

--- monitoring/update-urllists/kommersant-vrn.js
+++ monitoring/update-urllists/kommersant-vrn.js
@@ -4,7 +4,7 @@ var months=['01','02','03','04','05','06','07','08','09','10','11','12',];
 
 
 var extractor = new LinkExtractor({
-	flushEvery: 30*1000,
+	flushEvery: 1000,
 	name:       'kommersant-vrn',
 });
 
@@ -13,10 +13,11 @@ for(var year=2013; year<=2030; year++){
 	for(var i=0; i<12; i++){
 		extractor.extractURLlistFromURLsequence({
 			pagesCount:	1,
-			reportEvery: 10,
+			reportEvery: 1000,
 			prefix:	'https://www.kommersant.ru/archive/news/36/month/'+year+'-'+months[i]+'-01#',
 			linkpattern:	'/doc/',
 			linkprefix:	'https://www.kommersant.ru',
+			pause: 1000,
 		});
 	}
 }
