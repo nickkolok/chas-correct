1bcbe350d9ee429ab296d508f54aa3a41e416adf
aezv <aezv.global@gmail.com>
2021-04-28 20:36:10 +0300

    [monitoring] Скрипт сбора url'ов для vrntimes_ru

--- monitoring/update-urllists/vrntimes.js
+++ monitoring/update-urllists/vrntimes.js
@@ -1,13 +1,17 @@
 var LinkExtractor = require('../../parser/linkExtractor.js').LinkExtractor;
 
 var extractor = new LinkExtractor({
-	flushEvery: 60*1000,
-	name:       'vrntimes',
+	flushEvery: 2000,
+	name: 'vrntimes',
 });
 
+
 extractor.extractURLlistFromURLsequence({
-	pagesCount:	1500,
-	prefix:	'http://vrntimes.ru/frontpage?page=',
-	linkpattern:	'/articles/',
-	linkprefix:	'http://vrntimes.ru/print',
+	pagesCount: 500,
+	reportEvery: 2000,
+	flushEvery: 2000,
+	prefix: 'http://vrntimes.ru/frontpage?page=',
+	linkpattern: '/articles/',
+	linkprefix: 'http://vrntimes.ru/print',
+	pause: 2000
 });
