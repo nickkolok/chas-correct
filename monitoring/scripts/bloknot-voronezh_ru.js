53ba1fbebf54185e4bf20d3691e25d1f4f86ce03
aezv <aezv.global@gmail.com>
2021-04-28 18:04:40 +0300

    [monitoring] Скрипт мониторинга для bloknot-voronezh_ru

---
+++ monitoring/scripts/bloknot-voronezh_ru.js
@@ -0,0 +1,14 @@
+var urllist = require('../../parser/urllist.js');
+
+var name = 'bloknot-voronezh_ru';
+
+urllist.countErrorsInURLlist(
+	'urllists/' + name + '.urllist.json',
+	1000000,
+	/(<div class="news-item-info">)/i,
+	/(<div id="read-more" class="read-more width-full">)/i,
+	{
+		pause: 2000,
+		name: name,
+	}
+);
