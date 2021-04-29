08c90db3ebaac77258e53a7c27806e7b1a82b887
aezv <aezv.global@gmail.com>
2021-04-29 16:38:45 +0300

    [monitoring] (fix) Скрипт мониторинга для bloknot-voronezh_ru

--- monitoring/scripts/bloknot-voronezh_ru.js
+++ monitoring/scripts/bloknot-voronezh_ru.js
@@ -6,7 +6,7 @@ urllist.countErrorsInURLlist(
 	'urllists/' + name + '.urllist.json',
 	1000000,
 	/(<div class="news-item-info">)/i,
-	/(<div id="read-more" class="read-more width-full">)/i,
+	/(<b class="hideme">Новости на Блoкнoт-Воронеж<\/b>)/i,
 	{
 		pause: 2000,
 		name: name,
