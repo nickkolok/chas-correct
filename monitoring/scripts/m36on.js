1cf9e3b912c31c82f32746a5b3125a498a63c788
aezv <aezv.global@gmail.com>
2021-04-30 18:22:01 +0300

    [monitoring] Обновление скрипта мониторинга для m36on

--- monitoring/scripts/m36on.js
+++ monitoring/scripts/m36on.js
@@ -1,10 +1,15 @@
 var urllist = require('../../parser/urllist.js');
 
 //36on.ru - удобнее с мобильной версии
+var name = 'm36on';
+
 urllist.countErrorsInURLlist(
-	'urllists/m36on.urllist.json',
-	10000000,
-	"<div class='mobile_title l-page__content text'>",
-	"<div class='share_block'>",
-	{pause:500}
+	'urllists/' + name + '.urllist.json',
+	1000000,
+	/(<div class='mobile_title l-page__content text'>)/i,
+	/(<div class='share_block'>)/i,
+	{
+		pause: 500,
+		name: name,
+	}
 );
