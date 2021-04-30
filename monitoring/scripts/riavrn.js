ace68d20c7386b92b92ebd94d2f3a28e335e3866
aezv <aezv.global@gmail.com>
2021-04-30 20:10:04 +0300

    [monitoring] Форматирование скрипта мониторинга для riavrn_ru

--- monitoring/scripts/riavrn.js
+++ monitoring/scripts/riavrn.js
@@ -3,12 +3,12 @@ var urllist = require('../../parser/urllist.js');
 var name = 'riavrn';
 
 urllist.countErrorsInURLlist(
-    'urllists/' + name + '.urllist.json',
-    1000000,
-    /(<div class="article-intro">)/i,
-    /(<div class="article-info tag-tabs-info">)/i,
-    {
-        pause: 2000,
-        name: name,
-    }
+	'urllists/' + name + '.urllist.json',
+	1000000,
+	/(<div class="article-intro">)/i,
+	/(<div class="article-info tag-tabs-info">)/i,
+	{
+		pause: 2000,
+		name: name,
+	}
 );
