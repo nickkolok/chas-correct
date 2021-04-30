9afdd2df242f38727e0522571cd6bf0e5a0bd12c
aisse-258 <y-kate@ukr.net>
2021-04-30 16:14:56 +0300

    Ложноположительные vestivrn

---
+++ monitoring/falsepositives/vestivrn_ru.js
@@ -0,0 +1,6 @@
+module.exports = [
+	'о-вторых &ndash; ',
+	'о<a href="http://vestivrn.ru/news/2020/10/01/v-voronezhskoi-oblasti-vveli-rezhim-chs-iz-za-pozharov/" target="_blank">бъявлен',
+	'какие- то',
+	'«Евроцемент груп»',
+];
