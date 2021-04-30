6b0fcdd979ed42eac79b085b25f71f163bf7421b
aezv <aezv.global@gmail.com>
2021-04-30 20:01:29 +0300

    [monitoring] Форматирование скрипта сбора url'ов для vestivrn_ru (новости)

--- monitoring/update-urllists/vestivrn-novosti.js
+++ monitoring/update-urllists/vestivrn-novosti.js
@@ -1,20 +1,20 @@
 var LinkExtractor = require('../../parser/linkExtractor.js').LinkExtractor;
 
 var extractor = new LinkExtractor({
-    flushEvery: 2000,
-    name: 'vestivrn-novosti',
+	flushEvery: 2000,
+	name: 'vestivrn-novosti',
 });
 
 extractor.extractURLlistFromURLsequence({
-    pagesCount: 500,
-    reportEvery: 2000,
-    flushEvery: 2000,
-    prefix: 'https://vestivrn.ru/news/page/',
-    linkpattern: 'https://vestivrn.ru/news/',
-    linkprefix: '',
-    pause: 2000,
-    exclude: [
-        '/page/',
-        /\/\d*\/\d*\/\d*\/$/
-    ]
+	pagesCount: 500,
+	reportEvery: 2000,
+	flushEvery: 2000,
+	prefix: 'https://vestivrn.ru/news/page/',
+	linkpattern: 'https://vestivrn.ru/news/',
+	linkprefix: '',
+	pause: 2000,
+	exclude: [
+		'/page/',
+		/\/\d*\/\d*\/\d*\/$/
+	]
 });
