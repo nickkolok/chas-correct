90662e2a0d502f9ad32d3e7f70dd8d2fbca1e9ca
aisse-258 <y-kate@ukr.net>
2021-05-07 17:19:48 +0300

    Ложноположительные для vestivrn

--- monitoring/falsepositives/vestivrn-novosti.js
+++ monitoring/falsepositives/vestivrn-novosti.js
@@ -1,6 +1,9 @@
 module.exports = [
-	'о-вторых &ndash; ',
-	'о<a href="http://vestivrn.ru/news/2020/10/01/v-voronezhskoi-oblasti-vveli-rezhim-chs-iz-za-pozharov/" target="_blank">бъявлен',
+	/Во-вторых &ndash;/i,
+	'о|бъявлен',
 	'какие- то',
-	'«Евроцемент груп»',
+	'груп»',
+	'| ка',
+	'Чё происходит&raquo;',
+	'груп&raquo;',
 ];
