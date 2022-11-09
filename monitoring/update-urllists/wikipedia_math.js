ad705b55aacae1c60a1ee541acbda5dfb525014e
NickKolok <nickkolok@mail.ru>
2022-11-09 23:17:01 +0300

    [updurl] Wikipedia - remove duplicate library requirement

--- monitoring/update-urllists/wikipedia_math.js
+++ monitoring/update-urllists/wikipedia_math.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var request = require('request');
 
-var request = require('request');
-
 var reqstring=
     'https://petscan.wmflabs.org/?language=ru&project=wikipedia&depth=15&categories=%D0%9C%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0&search_max_results=500000&interface_language=en&active_tab=&format=json&sparse=on&doit=1'
 ;
